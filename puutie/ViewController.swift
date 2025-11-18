56ed2d9cdd7d632e1961217ca0a3e0c589f74bfb
gurhankuras <gurhankuras@hotmail.com>
2025-11-18 17:10:07 +0300

    Initial Commit

---
+++ puutie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  puutie
+//
+//  Created by Gurhan on 11/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ puutieTests/puutieTests.swift
@@ -0,0 +1,17 @@
+//
+//  puutieTests.swift
+//  puutieTests
+//
+//  Created by Gurhan on 11/18/25.
+//
+
+import Testing
+@testable import puutie
+
+struct puutieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
